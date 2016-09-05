ac7b7410a0c84a26195705d425cb2e577141eba2
Glen Maddern <glenmaddern@gmail.com>
2016-09-05 13:44:26 +1000

    ok got a basic implementation working

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,22 @@
+import path from 'path'
+import Server from './Server'
+import Crawler from './Crawler'
+import Writer from './Writer'
+
 export default () => {
-  console.log("omfg yes")
+  const baseDir = path.resolve('./build')
+  const writer = new Writer(baseDir)
+  writer.move('index.html', '200.html')
+
+  const server = new Server(baseDir, '/200.html', 2999)
+  server.start().then(() => {
+
+    const crawler = new Crawler("http://localhost:2999")
+    return crawler.crawl(({ path, html }) => {
+      const filename = `${path}${path.endsWith('/') ? 'index' : ''}.html`
+      console.log(`Saving ${path} as ${filename}`)
+      writer.write(filename, html)
+    })
+
+  }).then(() => server.stop(), err => console.log(err))
 }
