d23164f64511d74ad70d6deac0d10f41865e41ee
Glen Maddern <glenmaddern@gmail.com>
2017-05-24 23:36:36 +1000

    exporting an async wrapper and it's actually workingd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
+
+const state = {
+  requests: [],
+  data: window.react_snapshot_state || {},
+}
+let count = 0
 
 export const render = (rootComponent, domElement) => {
-  if (navigator.userAgent.match(/Node\.js/i) && window && window.reactSnapshotRender) {
-    domElement.innerHTML = ReactDOMServer.renderToString(rootComponent)
-    window.reactSnapshotRender()
+  ReactDOM.render(rootComponent, domElement)
+  if (navigator.userAgent.match(/Node\.js/i) && window && window.react_snapshot_render) {
+    window.react_snapshot_render(domElement, state)
+  }
+}
+
+export const async = func => {
+  const i = count++
+  const existing = state.data[i]
+  if (existing) {
+    console.log("SHORT CIRCUIT!")
+    return { then: x => x(...existing) }
   } else {
-    ReactDOM.render(rootComponent, domElement)
+    const promise = func().then((...response) => new Promise(resolve => {
+      state.data[i] = response
+      resolve(...response)
+    }))
+    state.requests.push(promise)
+    return promise
   }
 }
--- src/snapshot.js
+++ src/snapshot.js
@@ -1,10 +1,11 @@
 /* Wraps a jsdom call and returns the full page */
 
 import jsdom from 'jsdom'
+import * as ReactMarkupChecksum from 'react-dom/lib/ReactMarkupChecksum'
 
 export default (protocol, host, path, delay) => {
   return new Promise((resolve, reject) => {
-    let reactSnapshotRenderCalled = false
+    let render_called = false
     jsdom.env({
       url: `${protocol}//${host}${path}`,
       headers: { Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" },
@@ -23,17 +24,32 @@ export default (protocol, host, path, delay) => {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console),
       created: (err, window) => {
         if (err) reject(err)
-        window.reactSnapshotRender = () => {
-          reactSnapshotRenderCalled = true
-          setTimeout(() => {
-            resolve(window)
-          }, delay)
+        window.react_snapshot_render = (element, state) => {
+          render_called = { element, state }
         }
       },
       done: (err, window) => {
-        if (!reactSnapshotRenderCalled) {
-          reject("'render' from react-snapshot was never called. Did you replace the call to ReactDOM.render()?")
+        if (!render_called) {
+          return reject("'render' from react-snapshot was never called. Did you replace the call to ReactDOM.render()?")
         }
+
+        const { element, state } = render_called
+
+        const next = () => {
+          const shift = state.requests.shift()
+          return shift && shift.then(next)
+        }
+        /* Wait a short while, then wait for all requests, then serialise */
+        new Promise(res => setTimeout(res, delay))
+          .then(next)
+          .then(() => {
+            console.log(state.data)
+            element.outerHTML = ReactMarkupChecksum.addChecksumToMarkup(element.outerHTML)
+            window.document.body.insertAdjacentHTML('afterBegin', `
+              <script>window.react_snapshot_state = ${JSON.stringify(state.data)};</script>
+            `)
+            resolve(window)
+          })
       }
     })
   })
