cf8598beb0ec740b6a740b1a0edd4b0574809cf5
Glen Maddern <glenmaddern@gmail.com>
2017-05-30 17:01:54 +1000

    fleshed out the two snapshot apis that I want to go live with

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 export const IS_REACT_SNAPSHOT = navigator.userAgent.match(/Node\.js/i) && window && window.react_snapshot_render
 
@@ -15,19 +16,76 @@ export const render = (rootComponent, domElement) => {
   }
 }
 
-export const async = func => {
+export const snapshot = func => {
   const i = count++
   const existing = state.data[i]
   if (existing) {
-    console.log("SHORT CIRCUIT!")
-    return { then: x => x(...existing) }
+    const { success, failure } = existing
+    /* This mimics a Promise API but is entirely synchronous */
+    return {
+      then(resolve, reject) {
+        if (typeof success !== 'undefined') resolve(success)
+        else if (reject && typeof failure !== 'undefined') reject(failure)
+      },
+      catch(reject) {
+        if (typeof failure !== 'undefined') reject(success)
+      }
+    }
   } else {
     if (!IS_REACT_SNAPSHOT) return func()
-    const promise = func().then((...response) => new Promise(resolve => {
-      state.data[i] = response
-      resolve(...response)
-    }))
+    const promise = func().then(
+      success => {
+        state.data[i] = { success }
+        return success
+      },
+      failure => {
+        state.data[i] = { failure }
+        return Promise.reject(failure)
+      }
+    )
     state.requests.push(promise)
     return promise
+  }
+}
+
+export const Snapshot = (prop_defs) => {
+  const prop_names = Object.keys(prop_defs)
+  if (typeof prop_defs !== "object" ||
+    prop_names.some(k => typeof prop_defs[k] !== 'function')
+  ) throw new Error("Snapshot requires an object of type { propName: () => Promise }.")
+  console.log(prop_defs)
+
+  const hoc = (Component, render_without_data) => {
+    class SnapshotComponent extends React.Component {
+      constructor() {
+        super()
+        this.state = { loaded_all: false, async_props: null }
+      }
+
+      componentWillMount() {
+        snapshot(() =>
+          Promise.all(prop_names.map(prop_name => prop_defs[prop_name](this.props)))
+        ).then(responses => {
+          const new_state = {}
+          prop_names.forEach((prop_name, i) => new_state[prop_name] = responses[i])
+          this.setState({ async_props: new_state, loaded_all: true })
+        })
+      }
+
+      render() {
+        if (!this.state.loaded_all && !render_without_data) return null
+        const props = Object.assign({},
+          this.props,
+          this.state.async_props
+        )
+        return React.createElement(Component, props)
+      }
+    }
+    SnapshotComponent.displayName = `Snapshot(${Component.displayName || Component.name})`
+    return SnapshotComponent
+  }
+  return {
+    thenRender: Component => hoc(Component, false),
+    rendering: Component => hoc(Component, true)
   }
 }
